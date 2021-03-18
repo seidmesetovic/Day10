b2c16856d65b378185685bf34bfb4c92d1ae0b43
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-18 01:07:58 +0100

    ex08 gotov

---
+++ ex08/src/index.js
@@ -0,0 +1,22 @@
+function getUsername(email) {
+    var username = '';
+     var username = {
+
+        "user1@mail.com": "user1",
+        "user2@mail.com": "user2",
+        "user3@mail.com": "user3",
+        "user4@mail.com": "user4",
+        "user5@mail.com": "user5",
+        "user6@mail.com": "user6",
+    };
+    username = username[email];
+    return username;
+}
+getUsername("user1@mail.com");
+getUsername("user2@mail.com");
+getUsername("user3@mail.com");
+getUsername("user4@mail.com");
+getUsername("user5@mail.com");
+getUsername("user6@mail.com");
+getUsername("");
+module.exports = getUsername;
